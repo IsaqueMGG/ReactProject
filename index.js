369b662e4246d4e8dc4064c3cab660db5037f812
IsaqueMGG <100934859+IsaqueMGG@users.noreply.github.com>
2023-05-12 10:01:46 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'react-native';
+import {View, Text} from'react-native';
+
+export default function App(){
+  <View style={{flex:1}}>
+    <StatusBar style="auto"/>
+    <Text>Home</Text>
+  </View>
+}
